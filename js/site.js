f1919f1ff86df255342ace6d29382070359690ac
neilsatt <neilsatt@gmail.com>
2017-09-24 16:41:20 -0600

    remove btn var

--- js/site.js
+++ js/site.js
@@ -7,10 +7,7 @@ function showDate() {
 showDate();
 
 var modal = document.getElementById('myModal');          
-var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-
-
 span.onclick = function() {
     modal.style.display = "none";
 };
