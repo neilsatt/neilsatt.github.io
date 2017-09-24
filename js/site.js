0668ee276fc9c042c6bd4428630b462ccceb888c
neilsatt <neilsatt@gmail.com>
2017-09-24 16:39:45 -0600

    remove btn event listener

--- js/site.js
+++ js/site.js
@@ -9,9 +9,7 @@ showDate();
 var modal = document.getElementById('myModal');          
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-};
+
 
 span.onclick = function() {
     modal.style.display = "none";
