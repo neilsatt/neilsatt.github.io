1da5e8ffa6a75c01530585380a078ee422e09e5f
neilsatt <neilsatt@gmail.com>
2017-12-04 19:48:06 -0700

    update layout

--- js/site.js
+++ js/site.js
@@ -1,8 +1,9 @@
 var modal = document.getElementById('myModal');          
 var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-};
+
+//span.onclick = function() {
+//    modal.style.display = "none";
+//};
 
 window.onclick = function(event) {
     if (event.target == modal) {
